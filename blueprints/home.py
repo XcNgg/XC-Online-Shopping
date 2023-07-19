b1535daae5246d3624313585968f4d3dd38f2f8f
xcngg <1355523280@qq.com>
2023-07-19 21:45:21 +0800

    coding 2023.7.19 21.45 完成首页按照销量降序

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,5 +1,6 @@
-import flask
 from flask import Blueprint, request, render_template, redirect, g
+from extension import db
+from models import XcOSProduct
 
 home = Blueprint('home', __name__, url_prefix='/')
 
@@ -10,4 +11,17 @@ def index():
         'title': '小草Shopping-首页',
         'logo_title': '小草Shopping'
     }
-    return render_template('index.html', info=info)
+
+
+    # 查询并按销量降序排序，取前8个数据，同时满足 status=1 和 approval_status=1
+    top_products = (
+        XcOSProduct.query
+        .filter_by(status=1, approval_status=1)
+        .order_by(XcOSProduct.sales.desc())
+        .limit(8)
+        .all()
+    )
+    for p in top_products:
+        print(p.name)
+
+    return render_template('index.html', info=info,top_products=top_products)
